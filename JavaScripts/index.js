7eda0a20f9390a5f28cc8c299eb5fe0d49578fca
najoro <najofanantenana@gmail.com>
2022-10-24 22:47:44 +0300

    score

--- JavaScripts/index.js
+++ JavaScripts/index.js
@@ -1,14 +1,15 @@
 /**
   1. ajouter tous le niveaux que peut avoir les niveaux
   2.fonction si on click sur une niveaux il appareille sur le chiffre
- */
+*/
 
 //decalration...
 const btmRadios = document.querySelectorAll('input[type="radio"]');
 const numberYours = document.getElementById("numberYours");
 const numberMister = document.getElementById("numberMister");
 const valid = document.getElementById('valid-btm');
-
+const numbersContenaire = document.getElementById("numbers");
+const score = document.querySelector('.score')
 
 let mister = 0;
 let your = 0;
@@ -23,23 +24,21 @@ const levels = {
   level6: 203,
 };
 //functions...
-const utils = {
+const Utils = {
   levelActive: async function (arg) {
     levelTables.length = 0;
     for (let i = 0; i < arg + 1; i++) {
       levelTables.push(i);
     }
-    console.log(levelTables);
     this.displayLevel();
     this.displayNumberInCase();
     this.displayNumbreMIster();
   },
 
   displayLevel: function () {
-    const numbers = document.getElementById("numbers");
-    numbers.textContent = " ";
+    numbersContenaire.textContent = " ";
     levelTables.forEach((element) => {
-      numbers.innerHTML += `
+      numbersContenaire.innerHTML += `
     <div>
       <input type="checkbox" name="numberEatch" value = ${element} id="numberEatch">
       <label for="numberEatch" id="labelNumber">${element}</label>
@@ -60,25 +59,46 @@ const utils = {
     mister =  Math.ceil((Math.random() * levelTables.length) - 1);
 
   },
-
   testNumbre(){
     const indication = document.querySelector('.indication');
     function display(icone,transform) {
       indication.innerHTML = icone
       indication.style.transform = transform
     }
-    if (your === mister) {
+    function testSCore(){
+      if(score.textContent <= 0){
+        score.textContent = "50";
+        numbersContenaire.textContent = ` GAME OVER`;
+      }
+    }
+    function Notsame(rotate){
+      display("&#x27BC", `rotate(${rotate})`)
+      labelNumber[your].style.background = "red";
+      testSCore();
+      score.textContent -= Math.ceil((levelTables.length * 10)/100) 
+      setTimeout(() => {
+        display("&#x27BC", "rotate(0deg)")
+      }, 500);
+    }
+    function same() {
       numberMister.textContent = mister;
-      utils.displayNumbreMIster();
+      numbersContenaire.textContent = `Bravo... Next defie....`;
+      score.textContent = parseInt(score.textContent) + mister 
+      Utils.displayNumbreMIster();
       display("&#x2714", "rotate(0deg)")
+
       setTimeout(() => {
         indication.innerHTML = "&#x27BC"
-        numberMister.textContent = "?";
-     }, 3000);
+        numberMister.textContent = "?"
+        numberYours.textContent = "?"
+     }, 1000);
+    }
+    if (your === mister) {
+      same()
     }else if(your < mister){
-      display("&#x27BC", "rotate(-90deg)")
+      Notsame("-90deg")
     }else{
-      display("&#x27BC", "rotate(90deg)")
+      Notsame("90deg")
     }
   }
 };
@@ -87,11 +107,11 @@ btmRadios.forEach((btmRadio) => {
   btmRadio.addEventListener("click", () => {
     for (const level in levels) {
       if (btmRadio.id == level) {
-        utils.levelActive(levels[level]);
+        Utils.levelActive(levels[level]);
       }
     }
   });
 });
 valid.addEventListener('click', ()=> {
-  utils.testNumbre();
+  Utils.testNumbre();
 })
